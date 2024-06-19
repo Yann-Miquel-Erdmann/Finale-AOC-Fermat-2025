7503dbd6dc64bad2f1b139ebc5d3de03e84db8c4
Falaux-Bachelot Erwan <erwan.faba@gmail.com>
2024-06-19 18:55:11 +0200

    correction python

--- testing/quick_sort.py
+++ testing/quick_sort.py
@@ -1,11 +1,12 @@
 import random
+
 def swap(a, i0, i1):
     c = a[i1]
     a[i1] = a[i0]
-    a[i0] = 0
+    a[i0] = c
     return a
-    
-def partition (a, l0, hi):
+
+def partition(a, l0, hi):
     pivot = a[hi]
     i = l0
     for j in range(l0, hi):
@@ -17,7 +18,6 @@ def partition (a, l0, hi):
     b.append(i)
     b.append(a)
     return b
-            
 
 def quicksort(a, l0, hi):
     if l0 > hi or l0 < 0:
@@ -25,19 +25,19 @@ def quicksort(a, l0, hi):
     result = partition(a, l0, hi)
     p = result[0]
     a = result[1]
-    a = quicksort(a, p+1, hi)
-    a = quicksort(a, l0, p-1)
+    a = quicksort(a, p + 1, hi)
+    a = quicksort(a, l0, p - 1)
     return a
-    
+
 n = 5000
 l = []
 
-for i in range(1, n+1):
+for i in range(1, n + 1):
     l.append(i)
 
 random.seed(12)
-for i in range(n-1, -1, -1):
+for i in range(n - 1, -1, -1):
     j = random.random()
-    l = swap(i, j*n)
-
-l2 = quicksort(l, 0, n-1)
+    l = swap(l, i, int(j * n))
+    
+l2 = quicksort(l, 0, n - 1)
